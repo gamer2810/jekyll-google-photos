44eba916f9850edde292dba0462d2c29bd0a97ca
K3K <kha3ktran@gmail.com>
2025-02-13 11:44:22 -0600

    test: change nokogiri invocation

--- lib/jekyll-google-photos/tag.rb
+++ lib/jekyll-google-photos/tag.rb
@@ -13,7 +13,7 @@ module JekyllGooglePhotos
     end
 
     def getImageLinks(url)
-      doc = Nokogiri::HTML(Net::HTTP.get(URI(url.strip)))
+      doc = Nokogiri::HTML(URI::open(url.strip).read)
       scripts = doc.xpath("//script")
       jsonString = ""
       for x in scripts do
